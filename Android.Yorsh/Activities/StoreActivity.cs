20c34212944c4362f989be62fc18fd697adbaac8
Olga Krotova <o-krotowa@mail.ru>
2015-11-27 23:43:45 +0200

    fix the Store Activity

--- Android.Yorsh/Activities/StoreActivity.cs
+++ Android.Yorsh/Activities/StoreActivity.cs
@@ -10,44 +10,19 @@ namespace Android.Yorsh.Activities
 	[Activity(Label = "@string/BuyString", ParentActivity = typeof(MainMenuActivity), MainLauncher=true,ScreenOrientation = ScreenOrientation.Portrait)]
     public class StoreActivity : BaseActivity
     {
-<<<<<<< HEAD
-		string key = @"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr1wsRhQz/aFBC2HGRbIJ0Dx372QXWoSYBZT8nESCUl9dxhILH+dSS2UN43ovQsqAfb1WNQ+qvc8aVwNq0BXiVmNecqLYbsL2jZSGS7ox/Y8Xl03wsQfVHoOY6LDORvYN+ZqJxEuS1dqcUul4TiUqbemZKOAqNQeRUmMz/QmCnNiINuaOv3vOQKMyJsYPwqg+9oei/9bYSIPdkXiyHATbEIiMULheQrS0w4fMu8Fk6nqAMmp8RAXX/fCDCIoIkJ42kIpiUY9k8qeqHeZkrTVMi//AbuWK7Hx+4hhf6gIR4U4Mz544aXX93w56OTgnC32Ngqoa8RkzHpQPoS5dvLUMMwIDAQAB";
-		InAppBillingServiceConnection _serviceConnection;
-=======
-        InAppBillingServiceConnection _serviceConnection;
->>>>>>> 5a84c3f2f59dc6c83fe0a5ebae947182030bf130
-        protected override void OnCreate(Bundle bundle)
+		protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
             SetContentView(Resource.Layout.Store);
             var taskListView = FindViewById<ListView>(Resource.Id.taskListView);
-<<<<<<< HEAD
-            //taskListView.Adapter = new MultiItemRowListAdapter(this, new StoreListAdapter(), 3, 1);
-			char[] charArray = key.ToCharArray();
-			Array.Reverse( charArray );
-			var s = new string (charArray);
-//			_serviceConnection.OnConnected += () => {
-//				};
-//			_serviceConnection.BillingHandler.QueryInventoryAsync(
-//				new System.Collections.Generic.List<string> {
-//					ReservedTestProductIDs.Purchased,
-//					ReservedTestProductIDs.Canceled,
-//					ReservedTestProductIDs.Refunded,
-//					ReservedTestProductIDs.Unavailable
-//				}, ItemType.Product);
-//			// Attempt to connect to the service
-//			_serviceConnection.Connect ();
-=======
             taskListView.Adapter = new MultiItemRowListAdapter(this, new StoreListAdapter(), 3, 1);
             var m = @"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr1wsRhQz/aFBC2HGRbIJ0Dx372QXWoSYBZT8nESCUl9dxhILH+dSS2UN43ovQsqAfb1WNQ+qvc8aVwNq0BXiVmNecqLYbsL2jZSGS7ox/Y8Xl03wsQfVHoOY6LDORvYN+ZqJxEuS1dqcUul4TiUqbemZKOAqNQeRUmMz/QmCnNiINuaOv3vOQKMyJsYPwqg+9oei/9bYSIPdkXiyHATbEIiMULheQrS0w4fMu8Fk6nqAMmp8RAXX/fCDCIoIkJ42kIpiUY9k8qeqHeZkrTVMi//AbuWK7Hx+4hhf6gIR4U4Mz544aXX93w56OTgnC32Ngqoa8RkzHpQPoS5dvLUMMwIDAQAB";
-           
-            var key = Xamarin.InAppBilling.Utilities.Security.Unify
+           	var key = Xamarin.InAppBilling.Utilities.Security.Unify
                 (            
-                new string[]{ @"ifCDCIo", @"1wsRhIL",  @"MIIBIjA", @"iUqbeme",  @"3w56QAB", @"IkJaXX9",  @"HqvcsL2",   @"jZSGl4T" },
-                new int[]   {5, 1, 0, 4, 7, 6, 2, 3},
-                new string[]{@"i/9bYSIPdkXiyHATbEIiMULheQrS0w4fMu8Fk6nqAMmp8RAXX/fCDCIo", @"1wsRhQz/aFBC2HGRbIJ0Dx372QXWoSYBZT8nESCUl9dxhIL", @"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr", @"iUqbemZKOAqNQeRUmMz/QmCnNiINuaOv3vOQKMyJsYPwqg+9oe", @"3w56OTgnC32Ngqoa8RkzHpQPoS5dvLUMMwIDAQAB",@"IkJ42kIpiUY9k8qeqHeZkrTVMi//AbuWK7Hx+4hhf6gIR4U4Mz544aXX9", @"H+dSS2UN43ovQsqAfb1WNQ+qvc8aVwNq0BXiVmNecqLYbsL2", @"jZSGS7ox/Y8Xl03wsQfVHoOY6LDORvYN+ZqJxEuS1dqcUul4T"});
-            string.CompareOrdinal(m, key);
->>>>>>> 5a84c3f2f59dc6c83fe0a5ebae947182030bf130
+	                new string[]{ @"ifCDCIo", @"1wsRhIL",  @"MIIBIjA", @"iUqbeme",  @"3w56QAB", @"IkJaXX9",  @"HqvcsL2",   @"jZSGl4T" },
+	                new int[]   {5, 1, 0, 4, 7, 6, 2, 3},
+	                new string[]{@"i/9bYSIPdkXiyHATbEIiMULheQrS0w4fMu8Fk6nqAMmp8RAXX/fCDCIo", @"1wsRhQz/aFBC2HGRbIJ0Dx372QXWoSYBZT8nESCUl9dxhIL", @"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAr", @"iUqbemZKOAqNQeRUmMz/QmCnNiINuaOv3vOQKMyJsYPwqg+9oe", @"3w56OTgnC32Ngqoa8RkzHpQPoS5dvLUMMwIDAQAB",@"IkJ42kIpiUY9k8qeqHeZkrTVMi//AbuWK7Hx+4hhf6gIR4U4Mz544aXX9", @"H+dSS2UN43ovQsqAfb1WNQ+qvc8aVwNq0BXiVmNecqLYbsL2", @"jZSGS7ox/Y8Xl03wsQfVHoOY6LDORvYN+ZqJxEuS1dqcUul4T"});
+            var b = string.CompareOrdinal(m, key);
         }
 
 		private void Unify(string[] invert, int[] position)
